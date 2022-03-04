d6d317a8613a419cc60ffc935cc72d5eb84eea8d
Omar38 <omar.sultanov.98@mail.ru>
2022-03-04 08:51:29 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express();
 
-const port = 3000;
 
+//Template engine 
+app.set("view engine", "ejs");
+
+//Static files -Middlewares
+app.use(express.static("public"))
+
+//Routers
 app.get("/",(req,res)=>{
-    res.status(200).send("HOME PAGE")
+    res.status(200).render("index",{
+        page_name:'index'
+    })
 })
+
+app.get("/about",(req,res)=>{
+    res.status(200).render("about",{
+        page_name:"about"
+    })
+})
+
+const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is working with port:${port}`);
 })
